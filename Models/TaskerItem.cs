86ecee21021518b79bd3767ed07c4b9c4267f728
Bryan Cordova <bryancordova.hela@gmail.com>
2024-04-22 15:04:46 -0400

    Add project files.

---
+++ Models/TaskerItem.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskerBC.Models
+{
+    public class TaskerItem
+    {
+        public Guid Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public bool IsComplete { get; set; }
+    }
+}
---
+++ Services/TaskerItemService.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.JSInterop;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
+using TaskerBC.Models;
+
+namespace TaskerBC.Services
+{
+    public class TaskerItemService
+    {
+
+        //dependency injection in C#
+        private readonly IJSRuntime _jsRuntime;
+        public TaskerItemService(IJSRuntime jsRuntime) //constructor
+        { 
+            _jsRuntime = jsRuntime;
+        }
+        public async Task SaveTaskerItems(List<TaskerItem> taskerItems)
+        {
+            string itemsAsString = JsonSerializer.Serialize(taskerItems); 
+
+            await _jsRuntime.InvokeVoidAsync("localStorage.setItem", "TaskerItems", itemsAsString); 
+        }
+
+        public async Task<List<TaskerItem>> GetTaskerItems()
+        {
+            List<TaskerItem> taskerItems = new List<TaskerItem>();
+
+            try
+            {
+                string? tasksAsJson = await _jsRuntime.InvokeAsync<string?>("localStorage.getItem", "TaskerItems"); //<> type parameters
+
+                if (!string.IsNullOrEmpty(tasksAsJson))
+                {
+                    taskerItems = JsonSerializer.Deserialize<List<TaskerItem>>(tasksAsJson)!;
+
+                }
+            }
+            catch (Exception ex)
+            {
+                await SaveTaskerItems(taskerItems);
+
+                Console.WriteLine("Cold not load TaskerItems from local storage {0}", ex);
+            }
+
+            return taskerItems;
+        }
+    }
+
+
+}
